9485478af8ffe956042465a9a871b4b01953d7a6
Alex Insayt <mr.insayt@gmail.com>
2022-05-16 19:39:05 +0300

    fix

--- script/app.js
+++ script/app.js
@@ -99,3 +99,13 @@ $(document).ready(function(){
     centerMode: true,
   });
 });
+
+// Нажали на бургер
+$('#burger').on('click', function (e) {
+  $('.burger').show();
+});
+
+// Нажали на закрытие бургера
+$('#burger-close').on('click', function (e) {
+  $('.burger').hide();
+});
